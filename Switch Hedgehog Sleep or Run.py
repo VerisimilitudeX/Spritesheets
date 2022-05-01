9eb11debf531ae494ac3aa89a5b551369c2b3d52
Verisimilitude <acharyapiyush1@outlook.com>
2022-04-30 23:29:12 -0700

    Add files via upload

---
+++ Switch Hedgehog Sleep or Run.py
@@ -0,0 +1,40 @@
+"""
+LESSON: 5.2 - Spritesheets
+WARMUP 4
+"""
+
+import pygame
+import tsk
+pygame.init()
+
+window = pygame.display.set_mode([1018, 573])
+c = pygame.time.Clock()
+
+background = tsk.Sprite("OpenMeadow.jpg", 0, 0)
+sleep = tsk.ImageSheet("HedgehogSleep.png", 5, 6)
+run = tsk.ImageSheet("HedgehogRun.png", 5, 6)
+
+hedgehog = tsk.Sprite(run, 300, 200)
+
+drawing = True
+while drawing:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            drawing = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print("Switching")
+
+            # Switch between the image sheets
+            if hedgehog.image == run:
+                hedgehog.image = sleep
+            elif hedgehog.image == sleep:
+                hedgehog.image = run
+
+    hedgehog.update(c.get_time())
+    background.draw()
+    hedgehog.draw()
+
+    pygame.display.flip()
+    c.tick(30)
